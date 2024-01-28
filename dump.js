635811a1b620a1081f8ac6e80a8205d96111f82f
mksjohari <mksjohari@gmail.com>
2024-01-28 11:15:41 +0900

    only rm at begining

--- dump.js
+++ dump.js
@@ -1,8 +1,9 @@
 const { createClient } = require("@supabase/supabase-js");
 const { writeFileSync, mkdirSync, existsSync, rmSync } = require("fs");
 
-const supabaseUrl = "API_URL";
-const supabaseKey = "SERVICE_ROLE_JWT";
+const supabaseUrl = "https://mbobvuyfcdworlpwkvux.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ib2J2dXlmY2R3b3JscHdrdnV4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2MjE2ODc5MywiZXhwIjoxOTc3NzQ0NzkzfQ.ay0Es-oO1yUp7JCSdq9u3YhgrV54Vakee07mlU9GVfc";
 const options = {};
 const supabase = createClient(supabaseUrl, supabaseKey, options);
 async function dumpStorage() {
@@ -12,7 +13,7 @@ async function dumpStorage() {
     throw new Error(buckets.error);
   }
   let count = 0;
-  createDir("./supabase-dump");
+  createDir("./supabase-dump", true);
   await Promise.all(
     buckets.data.map(async (bucket) => {
       const rootLst = await storage.from(bucket.id).list();
@@ -31,7 +32,7 @@ async function dumpStorage() {
               if (folder.error) throw new Error(folder.error);
               await dump(folder.data, `${path}${d.name}/`, depth + 1);
             } else {
-              await createDir(`./supabase-dump/${bucket.id}/${path}`);
+              await createDir(`./supabase-dump/${bucket.id}/${path}`, false);
               console.log(`saving ${bucket.id}/${path}${d.name}`);
               writeFileSync(
                 `./supabase-dump/${bucket.id}/${path}${d.name}`,
@@ -49,9 +50,9 @@ async function dumpStorage() {
 }
 
 dumpStorage();
-async function createDir(path) {
+async function createDir(path, remove) {
   const exists = await existsSync(path);
-  if (exists) {
+  if (remove && exists) {
     await rmSync(path, { recursive: true, force: true });
   }
   await mkdirSync(path, { recursive: true });
